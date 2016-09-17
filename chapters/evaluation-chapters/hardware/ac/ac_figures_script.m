dabb12b636d4d9c5a76173b02f04ab678fc6ae39
Johnny Verhoeff <j.verhoeff@me.com>
2016-09-17 21:47:16 +0200

    Hardware feedback added

--- chapters/evaluation-chapters/hardware/ac/ac_figures_script.m
+++ chapters/evaluation-chapters/hardware/ac/ac_figures_script.m
@@ -5,16 +5,35 @@ load ac_data;
 
 e = 300;
 
-[ax,h1,h2] = plotyy(1:e, raw_ac_adc(1:e), 1:e, trigger(1:e));
+f = 10000;
+
+L = 127;
+
+time_vector = (0:(e-1)) ./f .* 1000;
+
+%[ax,h1,h2] = plotyy(time_vector, raw_ac_adc(1:e), time_vector, trigger(1:e));
+
+figure;
+plot(time_vector, raw_ac_adc(1:e));
+
+
 hold on; 
-plot([1 e],[mean(raw_ac_adc) mean(raw_ac_adc)], 'cyan');
+%plot([1 e],[mean(raw_ac_adc) mean(raw_ac_adc)], 'cyan');
 title('Incoming data');
-xlabel('Sample');
+ylabel('Raw ADC Values');
+xlabel('Time (ms)');
 
-set(get(ax(1),'Ylabel'),'String','Raw ADC Value') 
-set(get(ax(2),'Ylabel'),'String','Triggering Circuit Output') 
 
-legend('Raw ADC Values', 'Mean ADC Values', 'Trigger Circuit Output');
+yyaxis right;
+plot(time_vector, trigger(1:e));
+ylabel('Trigger Ouput');
+ylim([-.1 1.1]);
+
+%set(get(ax(1),'Ylabel'),'String','Raw ADC Value') 
+%set(get(ax(2),'Ylabel'),'String','Triggering Output') 
+
+%legend('Raw ADC Values', 'Mean ADC Values', 'Trigger Circuit Output');
+legend('Raw ADC Values', 'Trigger Output');
 
 
 new_idx = 1;
@@ -30,20 +49,21 @@ end;
 adc_dsp = abs(adc_dsp - mean(adc_dsp));
 
 figure;
-plot(adc_dsp(1:e));
+plot(time_vector, adc_dsp(1:e) .* 4 ./ 4095 ./ 2.8 .* 1000);
 title('Processed ADC Values');
-xlabel('Sample');
-ylabel('Scaled ADC Values');
+xlabel('Time (ms)');
+%ylabel('Scaled ADC Values');
+ylabel('Current (mA)');
 
 figure;
 hold on;
-plot(corr(1:e));
-plot(corr_not_included_code(1:e));
-plot([1 e],[127/2 127/2]);
+plot(time_vector, corr(1:e) ./ L);
+plot(time_vector, corr_not_included_code(1:e) ./ L);
+plot([time_vector(1) time_vector(end)],[1/2 1/2]);
 
-title('Correlation');
-xlabel('Sample');
-ylabel('Correlation number');
-legend('Corr. w/ present code', 'Corr. w/ not present code', 'Threshold');
+title('Identifying LED');
+xlabel('Time (ms)');
+ylabel('Code similarity');
+legend('Similarity w/ present code', 'Similarity w/ not present code', 'Threshold');
 
 hold off;
